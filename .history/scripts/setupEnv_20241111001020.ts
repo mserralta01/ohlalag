a33174b33e07b9a937ba5a95e404fb24d0c324eb
Matt Serralta <mserralta@gmail.com>
2024-11-11 06:41:28 -0500

    Stripe Publishable Keys in File
    Stripe Publishable Keys in File

--- .history/scripts/setupEnv_20241111001020.ts
+++ .history/scripts/setupEnv_20241111001020.ts
@@ -2,7 +2,7 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 const envContent = `VITE_MONGODB_URI=mongodb+srv://doadmin:7kL548A9CHJ1N3B6@db-mongodb-nyc3-47501-2d48b1f5.mongo.ondigitalocean.com/admin
-VITE_STRIPE_PUBLISHABLE_KEY=pk_live_51QJeHcIH0Nj8caTBcKr82egiNE6E3BuoFialu70DJBPGQcRaSk9tqwH8ELX9EhjdwfLIiUuxyuUwAIAMr34o6VaQ00kLCicG0s
+VITE_STRIPE_PUBLISHABLE_KEY=your_publishable_key_here
 VITE_JWT_SECRET=ohlala_jwt_secret_2024`;
 
 try {
